828c4a21aad9a91b5cff9b041b71c7d6d9beb634
NameIsNick <woosoungcho@gmail.com>
2022-11-24 21:14:12 +0900

    Update Codeup1088.c

---
+++ Codeup C Basic 100/Codeup1088.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    
+    scanf("%d", &a);
+
+    for (int i = 1; i <= a; i++) {
+        if (i % 3 == 0) continue;
+        else printf("%d ", i);
+    }
+
+    return 0;
+}
